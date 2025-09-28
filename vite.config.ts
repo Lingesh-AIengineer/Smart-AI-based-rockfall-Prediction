b88382a45f2c41d66955f1127f7fcc3eda490993
Gowtham Kumar~ <gowtham310106@gmai.com>
2025-09-28 19:32:48 +0530

    Added github pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,5 +15,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/Smart-AI-Based-Rockfall-Prediction",
+  base: "/Smart-AI-based-rockfall-Prediction/",
 }));
